701bed669c72faa51cc9d6500da279285d2bae1a
Andrey Boukreev <ruzoomparty@gmail.com>
2023-08-18 11:14:05 +0300

    new project

---
+++ start_boot/src/main/java/com/boukreev/template_spring_boot/controllers/SimpleController.java
@@ -0,0 +1,18 @@
+package com.boukreev.template_spring_boot.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SimpleController {
+
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/")
+    public String helloPage() {
+        return "hello";
+    }
+}
